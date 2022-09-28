72498b9a71b876af3e66b3dbbc42f75d3b79665a
campagnamatias <campagnamatias@gmail.com>
2022-09-28 00:39:04 -0300

    subo todo el tateti despues le pongo estilos mas lindos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [turn, setTurn] = useState('X')
+  const [grid, setGrid] = useState(Array(9).fill(null))
+  const [winner, setWinner] = useState('')
+
+  const win = useEffect(()=>{
+    winConditions()
+  }, [grid])
+
+  const winConditions = ()=>{
+    winByRow() 
+    winByCol() 
+    winByDiagonal() 
+  }
+
+  const winByRow = ()=>{
+    //first row
+    if(grid.slice(0,3).join('')==='XXX'){ setWinner('X'); return true }
+    if(grid.slice(0,3).join('')==='OOO'){ setWinner('O'); return true }
+    // second row
+    if(grid.slice(3,6).join('')==='XXX'){ setWinner('X'); return true } 
+    if(grid.slice(3,6).join('')==='OOO'){ setWinner('O'); return true }    
+    //third row
+    if(grid.slice(6,9).join('')==='XXX'){ setWinner('X'); return true }
+    if(grid.slice(6,9).join('')==='OOO'){ setWinner('O'); return true }
+    
+  }
+
+  const winByCol = ()=>{
+    //first col
+    const firstCol = `${grid[0]}${grid[3]}${grid[6]}`
+    if(firstCol==='XXX'){ setWinner('X'); return true}
+    if(firstCol==='OOO'){ setWinner('O'); return true}    
+    //second col
+    const secondCol = `${grid[1]}${grid[4]}${grid[7]}`
+    if(secondCol==='XXX'){ setWinner('X'); return true }
+    if(secondCol==='OOO'){ setWinner('O'); return true }    
+    //third col
+    const thirdCol = `${grid[2]}${grid[5]}${grid[8]}`
+    if(thirdCol==='XXX'){setWinner('X'); return true}
+    if(thirdCol==='OOO'){setWinner('O'); return true}
+  }
+
+  const winByDiagonal = ()=>{
+    //Diag 1
+    const diag1 = `${grid[0]}${grid[4]}${grid[8]}`
+    if(diag1==='XXX'){ setWinner('X'); return true}
+    if(diag1==='OOO'){ setWinner('O'); return true}    
+    //Diag 2
+    const diag2 = `${grid[2]}${grid[4]}${grid[6]}`
+    if(diag2==='XXX'){ setWinner('X'); return true }
+    if(diag2==='OOO'){ setWinner('O'); return true }
+  }
+
+  const play = (index)=>{
+    const newGrid = [...grid]
+    newGrid[index] = turn
+    setGrid(newGrid)
+
+    if(turn === 'X'){
+      setTurn('O')
+    }
+    else{
+      setTurn('X')      
+    }
+  }
+
+  const reset = ()=>{
+    setGrid(Array(9).fill(null))
+    setTurn('X')
+    setWinner('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        winner && <div className='winner'>El ganador es: {winner}</div>
+      }      
+      <div className={`grid ${winner}`}>
+        {
+          grid.map((cell, index)=>{
+            return <button key={index} className={`cell ${cell}`} onClick={()=>{play(index)}}>{cell}</button>
+          })        
+        }
+      </div>
+      <button className='reset' onClick={()=>{reset()}}>Reset</button>
     </div>
   );
 }
